9efc7df7db78dee9b8aace812f0e1f807648ed32
Justin Redd <justinredd@gmail.com>
2015-03-02 14:43:11 -0800

    Generated cookbook.

---
+++ metadata.rb
@@ -0,0 +1,8 @@
+name             'couchbase-cluster'
+maintainer       'The Authors'
+maintainer_email 'you@example.com'
+license          'all_rights'
+description      'Installs/Configures couchbase-cluster'
+long_description 'Installs/Configures couchbase-cluster'
+version          '0.1.0'
+
---
+++ recipes/default.rb
@@ -0,0 +1,5 @@
+#
+# Cookbook Name:: couchbase-cluster
+# Recipe:: default
+#
+# Copyright (c) 2015 The Authors, All Rights Reserved.
