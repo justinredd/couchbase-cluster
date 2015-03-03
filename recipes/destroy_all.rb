8bc7688031462d269f7e8f4a5d9e4cd7e316637b
Justin Redd <justinredd@gmail.com>
2015-03-02 16:38:16 -0800

    Fixed FC003 violation.

--- recipes/destroy_all.rb
+++ recipes/destroy_all.rb
@@ -1,6 +1,11 @@
 require 'chef/provisioning'
 
-machine_batch do
-  machines search(:node, '*:*').map { |n| n.name }
-  action :destroy
+if Chef::Config[:solo]
+  Chef::Log.warn('This recipe uses search. Chef Solo does not support ' +
+  'search. Try again with "chef-client -z recipes/destroy_all.rb".')
+else
+  machine_batch do
+    machines search(:node, '*:*').map { |n| n.name }
+    action :destroy
+  end
 end
